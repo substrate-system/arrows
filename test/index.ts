885e32d4ce0d6afd1abb98f234d39605a1d8b89a
nichoth <nichoth@nichoth.com>
2025-06-01 19:05:01 -0700

    init

--- test/index.ts
+++ test/index.ts
@@ -4,11 +4,11 @@ import '../src/index.js'
 
 test('example test', async t => {
     document.body.innerHTML += `
-        <{{component-name}} class="test">
-        </{{component-name}}>
+        <substrate-back class="test">
+        </substrate-back>
     `
 
-    const el = await waitFor('{{component-name}}')
+    const el = await waitFor('substrate-back')
 
     t.ok(el, 'should find an element')
 })
