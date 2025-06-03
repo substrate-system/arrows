a9da019b18b3e5a0c51dea260c22396443dfaf44
nichoth <nichoth@nichoth.com>
2025-06-02 20:46:32 -0700

    refactor

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,25 @@ declare global {
 class SubstrateInput extends WebComponent.create('substrate-input') {
     static observedAttributes = ['disabled']
 
+    get disabled ():boolean {
+        return !!(this.qs('button')?.hasAttribute('disabled'))
+    }
+
+    set disabled (value:boolean) {
+        const btn = this.qs('button')
+        if (value) {
+            // disable
+            btn?.setAttribute('disabled', '' + true)
+            if (this.hasAttribute('disabled')) return
+            this.setAttribute('disabled', '')
+        } else {
+            // enable
+            if (!this.hasAttribute('disabled')) return
+            this.removeAttribute('disabled')
+            btn?.removeAttribute('disabled')
+        }
+    }
+
     connectedCallback () {
         this.render()
     }
@@ -36,26 +55,6 @@ class SubstrateInput extends WebComponent.create('substrate-input') {
 export class SubstrateNext extends SubstrateInput {
     static NAME = 'substrate-next'
 
-    connectedCallback () {
-        this.render()
-    }
-
-    addEventListener<K extends keyof HTMLElementEventMap> (
-        type:K,
-        listener:(this:HTMLElement, ev:HTMLElementEventMap[K])=>any,
-        options?:boolean|AddEventListenerOptions
-    ):void {
-        return this.qs('button')?.addEventListener(type, listener, options)
-    }
-
-    removeEventListener<K extends keyof HTMLElementEventMap> (
-        type:K,
-        listener:(this:HTMLElement, ev:HTMLElementEventMap[K])=>any,
-        options?:boolean|AddEventListenerOptions
-    ):void {
-        return this.qs('button')?.removeEventListener(type, listener, options)
-    }
-
     render () {
         this.innerHTML = `<button>
             ${next}
@@ -66,42 +65,6 @@ export class SubstrateNext extends SubstrateInput {
 
 export class SubstrateBack extends SubstrateInput {
     static NAME = 'substrate-back'
-
-    /**
-     * Handle 'disabled' attribute changes
-     * @see {@link https://gomakethings.com/how-to-detect-when-attributes-change-on-a-web-component/#organizing-your-code Go Make Things article}
-     */
-    handleChange_disabled (oldValue:string, newValue:string) {
-        if (newValue === null) {
-            // was removed
-            this.disabled = false
-        } else {
-            this.disabled = true
-        }
-    }
-
-    get disabled ():boolean {
-        return !!(this.qs('button')?.hasAttribute('disabled'))
-    }
-
-    set disabled (value:boolean) {
-        const btn = this.qs('button')
-        if (value) {
-            // disable
-            btn?.setAttribute('disabled', '' + true)
-            if (this.hasAttribute('disabled')) return
-            this.setAttribute('disabled', '')
-        } else {
-            // enable
-            if (!this.hasAttribute('disabled')) return
-            this.removeAttribute('disabled')
-            btn?.removeAttribute('disabled')
-        }
-    }
-
-    connectedCallback () {
-        this.render()
-    }
 
     render () {
         this.innerHTML = `<button${this.disabled ? ' disabled' : ''}>
